5a105f03cdf39b6f09b5cbf66d302dca13948137
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-08-30 09:42:37 -0300

    Função inserirProduto
    Fazendo uma função para colocar produtos no Banco de dados

--- produtos/inserir.php
+++ produtos/inserir.php
@@ -1,9 +1,27 @@
 <?php
 require_once "../src/funcoes-fabricantes.php";
+require_once "../src/funcoes-produtos.php";
+
 $DadosFabricantes = lerFabricantes($conexao);
+
+if (isset($_POST["inserir"])) {
+    $nome = filter_input(INPUT_POST, "nome", FILTER_SANITIZE_SPECIAL_CHARS);
+    $preco = filter_input(INPUT_POST, "preco", FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+    $quantidade = filter_input(INPUT_POST, "quantidade", FILTER_SANITIZE_NUMBER_INT);
+
+    // Pegaremos o value,ou seja, o valor do id do fabricante
+    $fabricanteId = filter_input(INPUT_POST, "fabricante", FILTER_SANITIZE_NUMBER_INT);
+
+    $descricao = filter_input(INPUT_POST, "descricao", FILTER_SANITIZE_SPECIAL_CHARS);
+
+    inserirProduto($conexao,$nome,$preco,$quantidade,$fabricanteId,$descricao);
+    header("location:visualizar.php");
+};
+
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,6 +30,7 @@ $DadosFabricantes = lerFabricantes($conexao);
 
     </style>
 </head>
+
 <body>
     <h1>Produtos | INSERT</h1>
     <hr>
@@ -19,44 +38,45 @@ $DadosFabricantes = lerFabricantes($conexao);
     <hr>
     <p><a href="visualizar.php"></a></p>
 
-        <form action="" method="post">
-    <p>
-        <label for="nome">Nome:</label>
-        <input type="text" name="nome" id="nome" required>
-    </p>
+    <form action="" method="post">
+        <p>
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome" id="nome" required>
+        </p>
 
-    <p>
-        <label for="preco">Preço: </label>
-        <input type="number" min="10" max="10000" step="0.01" name="preco" id="preco" required>
-    </p>
+        <p>
+            <label for="preco">Preço: </label>
+            <input type="number" min="10" max="10000" step="0.01" name="preco" id="preco" required>
+        </p>
 
-    <p>
-    <label for="quantidade">Quantidade: </label>
-        <input type="number" min="10" max="10000" step="0.01" name="qunatidade" id="quantidade" required> 
-    </p>
+        <p>
+            <label for="quantidade">Quantidade: </label>
+            <input type="number" min="10" max="10000" step="0.01" name="quantidade" id="quantidade" required>
+        </p>
 
-    <p>
-        <label for="fabricante">Fabricante: </label>
-        <select requird name="fabricante" id="fabricante">
-            <option value=""></option>
-            <?php
-foreach($DadosFabricantes as $dados){ ?>
-  <option value="<?=$dados["id"]?>"><?=$dados["nome"]?></option>  
+        <p>
+            <label for="fabricante">Fabricante: </label>
+            <select requird name="fabricante" id="fabricante">
+                <option value=""></option>
+                <?php
+                foreach ($DadosFabricantes as $dados) { ?>
+                    <option value="<?= $dados["id"] ?>"><?= $dados["nome"] ?></option>
 
-<?php }
-?>
-        </select>
-    </p>
+                <?php }
+                ?>
+            </select>
+        </p>
 
-    <p>
-        <label for="descricao">Descrição: </label> <br>
-        <textarea name="descricao" id="descricao" cols="30" rows="3"></textarea>
-    </p>
+        <p>
+            <label for="descricao">Descrição: </label> <br>
+            <textarea name="descricao" id="descricao" cols="30" rows="3"></textarea>
+        </p>
 
-    <button type="submit">Inserir Produto</button>
+        <button type="submit" name="inserir">Inserir Produto</button>
 
-        </form>
-        <br>
-        <a href="visualizar.php">Voltar</a>
+    </form>
+    <br>
+    <a href="visualizar.php">Voltar</a>
 </body>
+
 </html>
--- src/funcoes-produtos.php
+++ src/funcoes-produtos.php
@@ -23,4 +23,32 @@ function lerProdutos(PDO $conexao): array
         die("Erro ao carregar produtos: " . $erro->getMessage());
     }
     return $resultado;
-};
+}; // Fim LerProdutos
+
+
+    function inserirProduto(PDO $conexao,string $nome,float $preco,int $quantidade,int $fabricanteId,string $descricao):void{
+        $sql = "INSERT INTO produtos(nome,preco,quantidade,descricao,fabricante_id) 
+        VALUES (:nome,:preco,:quantidade,:descricao,:fabricanteId)";
+
+        try {
+            $consulta = $conexao->prepare($sql);
+            $consulta->bindValue(":nome",$nome,PDO::PARAM_STR);
+
+            /* Ao trabalhar com valores "quebrados" para
+            os parâmetros nomeados, você deve usar a constante
+            PARAM_STR. no momento, não há outra forma no PDO de lidar
+            com valores deste tipo devido aos diferentes tipos de 
+            dados que cada Banco de Dados suporta. */
+            $consulta->bindValue(":preco",$preco,PDO::PARAM_STR);
+
+            $consulta->bindValue(":quantidade",$quantidade,PDO::PARAM_INT);
+            $consulta->bindValue(":descricao",$descricao,PDO::PARAM_STR);
+            $consulta->bindValue(":fabricanteId",$fabricanteId,PDO::PARAM_INT);
+            $consulta->execute();
+
+        } catch (Exception $erro) {
+            die("Erro ao inserir produto: ".$erro->getMessage());
+        }
+    };
+
+
