50b1ef7e324adf39ab44028c5c5258ed87bfbfed
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-08-31 09:18:55 -0300

    Update Criado
    Update para atualizar produtos baseado no id

--- src/funcoes-produtos.php
+++ src/funcoes-produtos.php
@@ -63,4 +63,22 @@ function lerUmproduto(PDO $conexao, int $produtoId): array
         die("Erro ao Selecionar: " . $erro->getMessage());
     }
     return $resultado;
+}; // Fim lerUmProduto
+
+function atualizarProduto(PDO $conexao,int $id,string $nome,float $preco,int $quantidade,string $descricao,int $fabricanteId):void{
+    $sql = "UPDATE produtos SET nome = :nome,preco = :preco,quantidade = :quantidade,descricao = :descricao,fabricante_id =:fabricanteId
+    WHERE id = :id";
+
+    try {
+        $consulta = $conexao->prepare($sql);
+        $consulta->bindValue(":nome",$nome,PDO::PARAM_STR);
+        $consulta->bindValue(":preco",$preco,PDO::PARAM_STR);
+        $consulta->bindValue(":quantidade",$quantidade,PDO::PARAM_INT);
+        $consulta->bindValue(":descricao",$descricao,PDO::PARAM_STR);
+        $consulta->bindValue(":fabricanteId",$fabricanteId,PDO::PARAM_INT);
+        $consulta->bindValue(":id",$id,PDO::PARAM_INT);
+        $consulta->execute();
+    } catch (Exception $erro) {
+        die("Erro ao atualizar: ".$erro->getMessage());
+    }
 };
